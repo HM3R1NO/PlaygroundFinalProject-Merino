4adb78fab2ec8a213138f8565109dbcd69a65466
Harold Merino <harold.merino@usach.cl>
2023-11-16 06:53:18 -0300

    primer_modelo

--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=250)
+    content=models.TextField()
+
+    def __str__(self): #De esta forma el elemento del modelo Post tendra el nombre del título ingresado
+        return self.title
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PlaygroundFinalProject-Merino
 Proyecto final CoderHouse (Pagina Web)
 
-super usuario:
-nombre: adminhms
-mail:harold.merino@usach.cl
-contraseña:hS95mS12
+super usuario
+    nombre: adminhms
+    mail:harold.merino@usach.cl
+    contraseña:hS95mS12
