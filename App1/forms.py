34fcdbb9d513d40221ab50dbf827f7306cd29755
Harold Merino <harold.merino@usach.cl>
2023-11-22 22:15:39 -0300

    de_cero

--- App1/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms 
-from .models import Post 
-
-class PostCreateForm(forms.ModelForm):
-    class Meta:
-        model=Post #hace referencia al modelo Post de .models. 
-                    #Por lo que los datos ingresados en el formulario darán a lugar a un objeto de esta clase (Post). 
-        fields=('title', 'content')
--- App1/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title=models.CharField(max_length=250)
-    content=models.TextField()
-
-    def __str__(self): #De esta forma el elemento del modelo Post tendra el nombre del título ingresado
-        return self.title
