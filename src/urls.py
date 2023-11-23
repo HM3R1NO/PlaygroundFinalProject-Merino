56e3d557ba05287430d23ffb5c1aca19d061948e
Harold Merino <harold.merino@usach.cl>
2023-11-23 00:40:39 -0300

    App_y_modelos_1

---
+++ src/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', 'templates/base.html', name='Home'),
+    path('measures/',include('publicaciones.urls', namespace='measures')), 
+    path('account/',include('usuarios.urls', namespace= 'account')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
